38baefcf479b75000bbce5710b10a7d33788c760
clockwo <39350729+clockwo@users.noreply.github.com>
2023-08-22 19:17:46 +0300

    feat: :sparkles: Remake div to button, add more functions in modules and update CSS

--- script.js
+++ script.js
@@ -20,9 +20,9 @@ Display controller:
 
 const gameBoard = (() => {
   const matrix = [
-    ['X', 'O', 'O'],
-    ['O', 'X', 'O'],
-    ['O', 'O', 'X'],
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
   ];
 
   const getMatrix = () => matrix;
@@ -30,4 +30,46 @@ const gameBoard = (() => {
   return { getMatrix };
 })();
 
-const displayController = (() => {})();
+const displayController = (() => {
+  const SQUARES_TO_RENDER = 9;
+
+  const selectors = {
+    board: '[data-js-board]',
+    square: '[data-js-square]',
+  };
+
+  const boardElement = document.querySelector(selectors.board);
+
+  const createSquareElement = (dataNumber) => {
+    //Create square
+    const squareElement = document.createElement('button');
+    squareElement.classList.add('square');
+    //Add span element for X or O
+    const spanElement = document.createElement('span');
+
+    squareElement.setAttribute(`data-js-square`, dataNumber);
+    squareElement.setAttribute(`type`, 'button');
+
+    squareElement.appendChild(spanElement);
+
+    return squareElement;
+  };
+
+  const renderSquareElements = () => {
+    for (let i = 1; i <= SQUARES_TO_RENDER; i++) {
+      boardElement.appendChild(createSquareElement(i));
+    }
+  };
+
+  const getBoardElement = () => boardElement;
+  const getSquareElements = () => document.querySelectorAll(selectors.square);
+
+  return { renderSquareElements, getSquareElements, getBoardElement };
+})();
+
+displayController.renderSquareElements();
+
+const boardElement = displayController.getBoardElement();
+boardElement.addEventListener('click', ({ target }) => {
+  if (!target.matches(`.square`)) return;
+});
