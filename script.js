0cdf011a4eebb31d146bbac6390f34ddee31b589
clockwo <39350729+clockwo@users.noreply.github.com>
2023-08-24 12:49:41 +0300

    feat: :sparkles: Add functionality for adding X by row and column

--- script.js
+++ script.js
@@ -1,75 +1,78 @@
-/* 
-
-
-Gameboard contains:
-Matrix:
-  [][][]  
-  [][][]
-  [][][]
-
-  Add value to matrix: X or O
-
-
-Display controller: 
-  render elements
-
-
-*/
-
-// Gameboard
-
-const gameBoard = (() => {
-  const matrix = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', ''],
-  ];
-
-  const getMatrix = () => matrix;
-
-  return { getMatrix };
-})();
-
 const displayController = (() => {
-  const SQUARES_TO_RENDER = 9;
-
   const selectors = {
-    board: '[data-js-board]',
-    square: '[data-js-square]',
+    board: "[data-js-board]",
+    square: "[data-js-square]",
   };
-
   const boardElement = document.querySelector(selectors.board);
 
-  const createSquareElement = (dataNumber) => {
-    //Create square
-    const squareElement = document.createElement('button');
-    squareElement.classList.add('square');
-    //Add span element for X or O
-    const spanElement = document.createElement('span');
-
-    squareElement.setAttribute(`data-js-square`, dataNumber);
-    squareElement.setAttribute(`type`, 'button');
-
-    squareElement.appendChild(spanElement);
-
+  const createSquareElement = (row, column) => {
+    const squareElement = document.createElement("button");
+    squareElement.classList.add("square");
+    squareElement.setAttribute("data-row", row);
+    squareElement.setAttribute("data-column", column);
+    squareElement.setAttribute("type", "button");
     return squareElement;
   };
 
   const renderSquareElements = () => {
-    for (let i = 1; i <= SQUARES_TO_RENDER; i++) {
-      boardElement.appendChild(createSquareElement(i));
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < 3; i++) {
+      // eslint-disable-next-line no-plusplus
+      for (let k = 0; k < 3; k++) {
+        boardElement.appendChild(createSquareElement(i, k));
+      }
     }
   };
 
+  const updateSquareElement = (row, column, value) => {
+    const updatedElement = document.querySelector(
+      `[data-row="${row}"][data-column="${column}"]`,
+    );
+    updatedElement.innerHTML = value;
+  };
+
   const getBoardElement = () => boardElement;
+
   const getSquareElements = () => document.querySelectorAll(selectors.square);
 
-  return { renderSquareElements, getSquareElements, getBoardElement };
+  return {
+    renderSquareElements,
+    getSquareElements,
+    getBoardElement,
+    updateSquareElement,
+  };
+})();
+
+// Game board
+
+const gameBoard = (() => {
+  const matrix = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+
+  let lastUpdatedValue = ["", "", ""];
+
+  const setValue = (row, column) => {
+    matrix[row][column] = "X";
+    lastUpdatedValue = [row, column, "X"];
+  };
+
+  const getLastUpdatedValue = () => lastUpdatedValue;
+  const getMatrix = () => matrix;
+
+  return { getMatrix, setValue, getLastUpdatedValue };
 })();
 
 displayController.renderSquareElements();
 
 const boardElement = displayController.getBoardElement();
-boardElement.addEventListener('click', ({ target }) => {
-  if (!target.matches(`.square`)) return;
+boardElement.addEventListener("click", ({ target }) => {
+  if (!target.matches(".square")) return;
+
+  const targetRow = +target.dataset.row;
+  const targetColumn = +target.dataset.column;
+  gameBoard.setValue(targetRow, targetColumn);
+  displayController.updateSquareElement(...gameBoard.getLastUpdatedValue());
 });
